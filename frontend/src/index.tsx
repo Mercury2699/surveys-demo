45c26474b63a55378852d9f10b5e9b376ba1f92a
Jake He <junjie526@outlook.com>
2021-09-26 23:04:01 -0400

    auto adapt dark ui

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,37 @@
-import * as React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
 import App from './App';
-import theme from './theme';
+import { createTheme, useMediaQuery } from '@mui/material';
+
+const Index = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = createTheme({
+    palette: {
+      mode: prefersDarkMode ? 'dark' : 'light',
+      primary: {
+        main: '#556cd6',
+      },
+      secondary: {
+        main: '#19857b',
+      },
+      error: {
+        main: red.A400,
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  )
+}
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+  <Index/>,
   document.querySelector('#root'),
 );
